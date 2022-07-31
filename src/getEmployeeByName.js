c0a64b1f59c062d0d126fc6591093bf5abb153b4
Antônio Santana <antonionetosg6@gmail.com>
2022-07-31 12:27:50 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  const findColaborator = data.employees.find((item) => (item.firstName.includes(employeeName)
+  || item.lastName.includes(employeeName) ? item : undefined));
+  return findColaborator;
 }
+
+console.log(getEmployeeByName('Wilburn'));
 
 module.exports = getEmployeeByName;
