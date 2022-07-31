b167148513dc698430bd63688c9e35c416d5d7b1
Antônio Santana <antonionetosg6@gmail.com>
2022-07-31 11:42:13 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const speciesById = species.filter((item) => ids.includes(item.id));
+  return speciesById;
 }
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
