7b66436a6390872896750e276926e208f58e1909
Antônio Santana <antonionetosg6@gmail.com>
2022-08-02 20:55:50 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,34 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const entrantsQuant = {
+    child: entrants.filter((i) => i.age < 18).length,
+    adult: entrants.filter((i) => i.age >= 18 && i.age < 50).length,
+    senior: entrants.filter((i) => i.age >= 50).length,
+  };
+  return entrantsQuant;
+}
+function calculateEntry(entrants) {
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+
+  const priceObj = countEntrants(entrants);
+
+  const { child } = priceObj;
+  const { adult } = priceObj;
+  const { senior } = priceObj;
+
+  return ((child * 20.99) + (adult * 49.99) + (senior * 24.99));
 }
 
-function calculateEntry(entrants) {
-  // seu código aqui
-}
+console.log(calculateEntry([
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+]));
 
 module.exports = { calculateEntry, countEntrants };
