2e9b117e74ee688b662a497aa0716751c20b2e0e
Antônio Santana <antonionetosg6@gmail.com>
2022-08-02 07:42:04 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,20 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) => employee.managers.find((item) => item === id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === true) {
+    const colaboratorPerson = (employees.filter((employee) =>
+      employee.managers.some((person) => person === managerId)));
+    return colaboratorPerson.map((personTwo) => `${personTwo.firstName} ${personTwo.lastName}`);
+  }
+
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
+
+console.log(getRelatedEmployees(isManager('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1')));
 
 module.exports = { isManager, getRelatedEmployees };
