cfb1d4dde8d1583f10a16fe87990edef501e88e6
Antônio Santana <antonionetosg6@gmail.com>
2022-08-02 13:07:18 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal || undefined) {
+    const animalObj = {};
+    data.species.forEach((i) => {
+      animalObj[i.name] = i.residents.length;
+    });
+    return animalObj;
+  }
+  const specificResidents = data.species.find((i) => i.name === animal.specie).residents; // Retorna os residentes da espécie
+
+  const specificResidentsBySex = specificResidents.filter((i) => i.sex === animal.sex);
+
+  if (Object.entries(animal).length === 1) {
+    return specificResidents.length;
+  }
+  return specificResidentsBySex.length;
+
 }
 
+console.log(countAnimals({ specie: 'lions' }));
+
 module.exports = countAnimals;
+
+
