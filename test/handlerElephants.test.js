bc07d7f51413a2c0fced265f513bab27c5a76e42
Antônio Santana <antonionetosg6@gmail.com>
2022-08-02 17:24:49 -0300

    Requisito 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,17 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+    it('Testa se handlerElephants() retorna undefined', () => {
+        expect(handlerElephants()).toBeUndefined();
+    })
+    it('Testa se handlerElephants("count") retorna o número de residentes', () => {
+        expect(handlerElephants('count')).toEqual(4);
+    })
+    it('Testa se handlerElephants("names") retorna o esperado', () => {
+        const expected = ['Ilana', 'Orval', 'Bea', 'Jefferson']
+        expect(handlerElephants('names')).toEqual(expected);
+    })
+    it('Testa se handlerElephants("averageAge") retorna a média esperada', () => {
+        expect(handlerElephants('averageAge')).toEqual(10.5);
+    })
+});
