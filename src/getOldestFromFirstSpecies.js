50e80357d0a712661a59d2b5899139686d636635
Antônio Santana <antonionetosg6@gmail.com>
2022-08-03 09:46:34 -0300

    Requisito 10[solved], requisito 9[unsolved]

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+function getOldestFromFirstSpecies(personId) {
+
+  const specieId = data.employees.find(({ id }) => id === personId).responsibleFor[0];
+
+  const animals = data.species.find(({ id }) => id === specieId).residents;
+
+  const oldestAnimal = animals.sort((a, b) => b.age - a.age)[0];
+
+  return Object.keys(oldestAnimal).map((i) => oldestAnimal[i]);
 }
+
+
+console.log(getOldestFromFirstSpecies('56d43ba3-a5a7-40f6-8dd7-cbb05082383f'));
 
 module.exports = getOldestFromFirstSpecies;
